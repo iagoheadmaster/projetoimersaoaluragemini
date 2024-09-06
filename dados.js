cb375afdad199a03b0a337f5836552de8a79d83c
Iago Menezes Soares <167209965+iagoheadmaster@users.noreply.github.com>
2024-09-06 15:59:03 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,7 +3,7 @@ let dados = [
       titulo: "Rayssa Leal",
       descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
       link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
-      tags: "skate fada sk8 raissa raiça raisa"
+      tags: "skate skate street fada sk8 raissa raiça raisa"
     },
     {
       titulo: "Beatriz Souza",
@@ -16,5 +16,47 @@ let dados = [
       descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas em competições internacionais, destacando-se pela sua força e elegância. Ela é considerada uma das maiores ginastas da história do Brasil.",
       link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
       tags: "ginastica ginástica ouro rebecca rebeka rebecka"
-    }
+    },
+    {
+      titulo: "Izaquias Queiroz",
+      descricao: "Izaquias Queiroz é um canoísta brasileiro, conhecido por sua força e técnica na canoagem velocidade. Ele conquistou diversas medalhas em competições internacionais, representando o Brasil com maestria.",
+      link: "https://pt.wikipedia.org/wiki/Izaquias_Queiroz",
+      tags: "canoagem canoista velocidade izaquias queiroz isaquias queiros canoagem "
+  },
+  {
+      titulo: "Gabriel Medina",
+      descricao: "Gabriel Medina é um surfista profissional brasileiro, conhecido por suas manobras radicais e estilo único. Ele é um dos principais nomes do surf mundial e já conquistou diversos títulos importantes.",
+      link: "https://pt.wikipedia.org/wiki/Gabriel_Medina",
+      tags: "surf surfista gabriel medina radical ondas onda prancha"
+  },
+  {
+      titulo: "Alison dos Santos",
+      descricao: "Alison dos Santos é um atleta brasileiro especializado em provas de curta distância com barreiras. Ele é conhecido por sua velocidade e técnica, e já conquistou diversas medalhas em competições internacionais.",
+      link: "https://pt.wikipedia.org/wiki/Alison_dos_Santos",
+      tags: "atletismo atletismo brasileiro alison dos santos alisson alissom alyson alysson alyssom"
+  },
+  {
+      titulo: "Ana Sátila",
+      descricao: "Ana Sátila é uma canoísta brasileira especializada em canoagem slalom. Ela é uma das principais atletas brasileiras na modalidade e já conquistou diversas medalhas em competições internacionais.",
+      link: "https://pt.wikipedia.org/wiki/Ana_Sátila",
+      tags: "canoagem slalom ana sátila canoa satila satilla sátilla canoa canoista"
+  },
+  {
+      titulo: "Flávia Saraiva",
+      descricao: "Flávia Saraiva é uma ginasta brasileira que se destacou nas competições internacionais, principalmente nas provas de solo e trave. Ela é conhecida por sua elegância e dificuldade nas execuções.",
+      link: "https://pt.wikipedia.org/wiki/Flávia_Saraiva",
+      tags: "ginastica flavia saraiva ginástica flavinha"
+  },
+  {
+      titulo: "Augusto Akio",
+      descricao: "Augusto Akio, também conhecido como Japinha, é um skatista brasileiro especializado em skate park. Ele é conhecido por suas manobras técnicas e fluidas, e já conquistou diversos títulos importantes no cenário nacional e internacional.",
+      link: "https://pt.wikipedia.org/wiki/Augusto_Akio",
+      tags: "skate skate park augusto akio japinha japa sk8"
+  },
+  {
+      titulo: "Pedro Barros",
+      descricao: "Pedro Barros é um skatista brasileiro especializado em skate park. Ele é conhecido por suas manobras inovadoras e estilo único, e já conquistou diversos títulos importantes no cenário mundial.",
+      link: "https://pt.wikipedia.org/wiki/Pedro_Barros",
+      tags: "skate skate park sk8"
+  }
   ];
